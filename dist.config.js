e8960d0bf314b55c17e0f28b1d50d661c6c4b97b
Chris Aquino <chris@radishmouse.com>
2021-02-04 18:42:18 +0000

    adds example config

---
+++ dist.config.js
@@ -0,0 +1,13 @@
+// acquire a token here: https://github.com/settings/tokens
+const token = 'token-goes-here';
+
+// list repos here like so:
+const repos = [
+  'radishmouse/commit-snooper'
+];
+
+module.exports = {
+  repos,
+  token
+};
+
